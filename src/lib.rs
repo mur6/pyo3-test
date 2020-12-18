26821283cadd6409b6eb709944f328c0277c47d7
MURAKI Taichi <muraki.taichi@gmail.com>
2020-12-18 09:38:25 +0900

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,20 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-/// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
+}
+
+#[pyfunction]
+fn multiply_array(a: Vec<usize>, b: usize) -> PyResult<Vec<usize>> {
+    Ok(a.iter().map(|v| v * b).collect())
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyo3_test(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(multiply_array, m)?)?;
     Ok(())
 }
